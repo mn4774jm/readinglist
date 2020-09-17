ec29241ee2b2eb61ceb9a4657d37c1796358ead0
NateBigStone <n.gaut@bigstonemgmt.com>
2020-09-17 18:46:12 -0500

    fix-crash-when-adding-duplicate-book

--- main.py
+++ main.py
@@ -32,9 +32,11 @@ def create_menu():
 
 
 def add_book():
-    new_book = ui.get_book_info()
-    new_book.save()
-    
+    try:
+        new_book = ui.get_book_info()
+        new_book.save()
+    except Exception as e:
+        print(f'\n{e}\n')
 
 def show_read_books():
     read_books = store.get_books_by_read_value(True)
